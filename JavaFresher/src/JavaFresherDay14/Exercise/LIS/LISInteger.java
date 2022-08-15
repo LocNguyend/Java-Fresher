dc25a992d7ea7fe9348b13ab5e3d6c40c965d32d
Loc <locnguyen5801@gmail.com>
2022-08-15 15:40:18 +0700

    Day14

--- JavaFresher/src/JavaFresherDay14/Exercise/LIS/LISInteger.java
+++ JavaFresher/src/JavaFresherDay14/Exercise/LIS/LISInteger.java
@@ -10,40 +10,39 @@ public class LISInteger {
                 return;
             }
 
-            // LIS[i] stores the longest increasing subsequence of subarray
-            // `arr[0…i]` that ends with `arr[i]`
+            // LIS[i] chua day con dai nhat ket thuc boi arr[i] tu phan tu ban dau -> i`
             List<List<Integer>> LIS = new ArrayList<>();
             for (int i = 0; i < arr.length; i++) {
                 LIS.add(new ArrayList<>());
             }
 
-            // LIS[0] denotes the longest increasing subsequence ending at `arr[0]`
+            // LIS[0] khai bao day con dai nhat o `arr[0]`
             LIS.get(0).add(arr[0]);
 
-            // start from the second array element
+            // bat dau tu phan tu thu 2
             for (int i = 1; i < arr.length; i++)
             {
-                // do for each element in subarray `arr[0…i-1]`
+                // lap lai tu phan tu dau tien toi no `arr[0…i-1]`
                 for (int j = 0; j < i; j++)
                 {
-                    // find the longest increasing subsequence that ends with `arr[j]`
-                    // where `arr[j]` is less than the current element `arr[i]`
+                    // tim day con dai nhat ket thuc o `arr[j]`
+                    // khi `arr[j]` nho hon phan tu dang xet `arr[i]`
 
                     if (arr[j] < arr[i] && LIS.get(j).size() > LIS.get(i).size()) {
                         LIS.set(i, new ArrayList<>(LIS.get(j)));
                     }
                 }
 
-                // include `arr[i]` in `LIS[i]`
+                // add `arr[i]` vao trong `LIS[i]`
                 LIS.get(i).add(arr[i]);
             }
 
-            // uncomment the following code to print contents of `LIS`
+            // in ra cac phan tu co trong `LIS`
         /*for (int i = 0; i < arr.length; i++) {
             System.out.println("LIS[" + i + "] — " + LIS.get(i));
         }*/
 
-            // `j` will store an index of LIS
+            // `j` chua max cua LIS
             int j = 0;
             for (int i = 0; i < arr.length; i++)
             {
--- JavaFresher/src/JavaFresherDay14/Exercise/LIS/LISString.java
+++ JavaFresher/src/JavaFresherDay14/Exercise/LIS/LISString.java
@@ -15,35 +15,32 @@ public class LISString {
             return;
         }
 
-        // LIS[i] stores the longest increasing subsequence of subarray
-        // `arr[0…i]` that ends with `arr[i]`
         List<List<Character>> LIS = new ArrayList<>();
         for (int i = 0; i < arr.size(); i++) {
             LIS.add(new ArrayList<>());
         }
 
-        // LIS[0] denotes the longest increasing subsequence ending at `arr[0]`
+
         LIS.get(0).add(arr.get(0));
 
-        // start from the second array element
+
         for (int i = 1; i < arr.size(); i++)
         {
-            // do for each element in subarray `arr[0…i-1]`
+
             for (int j = 0; j < i; j++)
             {
-                // find the longest increasing subsequence that ends with `arr[j]`
-                // where `arr[j]` is less than the current element `arr[i]`
+
 
                 if (arr.get(j) < arr.get(i) && LIS.get(j).size() > LIS.get(i).size()) {
                     LIS.set(i, new ArrayList<>(LIS.get(j)));
                 }
             }
 
-            // include `arr[i]` in `LIS[i]`
+
             LIS.get(i).add(arr.get(i));
         }
 
-        // uncomment the following code to print contents of `LIS`
+        
         /*for (int i = 0; i < arr.length; i++) {
             System.out.println("LIS[" + i + "] — " + LIS.get(i));
         }*/
